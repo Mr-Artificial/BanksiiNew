66428fed58bac2d0c886c3d9df60f8ec712bc5e9
maxjackson <maxim.jackson@live.com>
2022-08-19 13:08:43 -0700

    final Update v2

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping} from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
